0dad1000ba707a28a9d9e760804b204a8d2cfc49
lulu <1132923539@qq.com>
2020-09-04 02:04:37 +0800

    py远程调试

--- test/main.py
+++ test/main.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from inspect import isfunction
 import types
 import sys
@@ -9,8 +10,10 @@ import sys
 # t1\\venv27\\Scripts', 'C:\\Python27\\Lib', 'C:\\Python27\\DLLs', 'C:\\Python27\\Lib\\lib-tk', 'G:\\py_project\\pytest1\\venv27', 'G:\\py_
 # project\\pytest1\\venv27\\lib\\site-packages', 'G:\\py_project\\pytest1\\venv27\\lib\\site-packages\\pydevd-pycharm.egg']
 def debug():
+    sys.path.append("C:/Users/11329/Desktop/pydevd-pycharm")
     import pydevd
     pydevd.settrace('localhost', port=11111, stdoutToServer=True, stderrToServer=True)
+    print sys.executable
     print sys.path
     print "aaaaa"
     print "aaaaa"
